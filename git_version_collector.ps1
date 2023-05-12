0d83ab3bbf32cc71b4c86c760a29d55f9490bd97
johnnychang924 <72657726+johnnychang924@users.noreply.github.com>
2023-05-13 02:56:21 +0800

    Update git_version_collector.ps1
    add tar function、fix bug

--- git_version_collector.ps1
+++ git_version_collector.ps1
@@ -2,8 +2,10 @@
 $git_address = "https://github.com/tensorflow/tensorflow.git" #git repo的複製地址
 $version_num = 25 							#總共要收集的版本數量
 $gap = 3000									#每個版本間要相差多少版本
+$folder = "tensorflow"
 git clone $git_address
-$folder = Get-ChildItem -Path . -Directory			#抓取git repo名稱，資料夾中如果有其他檔案可能會造成抓錯
+$tar_folder = $folder + "_tar"
+New-Item -Path . -Name $tar_folder -ItemType "directory"
 for ($i = 1; $i -le $version_num; $i++){
 	Copy-Item "./$folder" -Destination "./$i" -Recurse
 	cd $i
@@ -11,4 +13,9 @@ for ($i = 1; $i -le $version_num; $i++){
 	git checkout head~$temp
 	Remove-Item "./.git" -Recurse -Force			#刪除.git檔案
 	cd ..
+	echo "./$tar_folder/$i.tar"
+	tar -cvf "./$tar_folder/$i.tar" "./$i"
+	Remove-Item "./$i" -Recurse -Force
 }
+Remove-Item "./$folder" -Recurse -Force
+pause
